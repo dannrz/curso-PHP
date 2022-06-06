5fad15bda5e61717a0c6634fefb1a8a01b6ee3af
dannrz <rozd.21d97@gmail.com>
2022-06-06 17:15:18 +0000

    Repaso y agregacion de controlador de base de datos

--- MVC/config/database.php
+++ MVC/config/database.php
@@ -1,10 +1,13 @@
-<?php 
-class database {
-    public static function conectar(){
-        $conexion = new mysqli("192.168.68.20","tesseract", "HexOmeg@96", "notas_master");
+<?php
+class database
+{
+    public static function conectar()
+    {
+        $conexion = new mysqli("localhost", "tesseract", "HexOmeg@96", "notas_master");
         $conexion->query("set names 'utf8'");
 
         return $conexion;
     }
 }
+
 ?>
--- MVC/controllers/UsuarioController.php
+++ MVC/controllers/UsuarioController.php
@@ -4,7 +4,7 @@ class UsuarioController{
         require_once 'models/usuario.php';
 
         $usuario = new Usuario();
-        $todos_los_usuarios = $usuario->conseguirTodos();
+        $todos_los_usuarios = $usuario->conseguirTodos('usuarios');
         require_once 'views/usuarios/mostrar-todos.phtml';
     }
     public function crear(){
